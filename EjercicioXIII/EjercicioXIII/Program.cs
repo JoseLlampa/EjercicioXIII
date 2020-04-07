a14d556b80878d48b502129f77ba0a0400c3557f
JoseLlampa <62297243+JoseLlampa@users.noreply.github.com>
2020-04-06 23:05:12 -0300

    Factoreo de todos los numeros enteros positivo

---
+++ EjercicioXIII/EjercicioXIII/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioXIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            bool error = false;
+            int factorial = 1;
+
+            Console.WriteLine("Ingrese un numero para calcular el factorial");
+
+            do
+            {
+                do
+                {
+                    error = false;
+                    if (!int.TryParse(Console.ReadLine(), out numero))
+                    {
+                        Console.WriteLine("No se puede aplicar factorial sobre una cadena no numerica");
+                        error = true;
+                    }
+
+                } while (error);
+
+                if (numero < 0)
+                {
+                    Console.WriteLine("No se puede calcular factoria para numeros negativos");
+                }
+            } while (numero<0);
+
+            for(int i = 1; i<= numero; i++)
+                {
+                    factorial = factorial * i;
+                }
+            
+            Console.WriteLine("El factorial de "+numero+" es igual a "+factorial);
+            
+            Console.ReadKey();
+
+        }
+    }
+}
